04585ff62a0e1e7e5c32a9cc7014a29374c9080b
ahad rafat talebi <rafat@bornamehr.com>
2020-02-29 15:21:03 +0330

    v1.0.65 index.d.ts file fixed for better support of typeScript target

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,10 @@ import { AsService } from '@barteh/as-service';
 
 
 export type TServiceStatus='error' | 'ready' | 'loading';
-
-export function useService(service:AsService,...params:Array<any>):[TServiceStatus,any,any,(void=>void)];
+export type TUseServiceRetType={status:TServiceStatus,data:any,error:any,retry:()=>void};
+export function useService(service:AsService,...params:Array<any>):TUseServiceRetType;
 
 export type TActionStatus='idle' | 'busy' | 'error';
-export function useAction(service:AsService,...params:Array<any>):[TActionStatus,any,any,(any=>any)];
+export type TUseActionRetType={status:TActionStatus,data:any,error:any,run:(...param:any[])=>any};
+
+export function useAction(service:AsService,...params:Array<any>):TUseActionRetType;
